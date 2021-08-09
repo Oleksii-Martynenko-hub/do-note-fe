3acc581d5e8d3eda2c65a02a175bcf06bbc5c2dc
Oleksii Martynenko <4lexm4rtin@gmail.com>
2021-08-09 17:51:13 +0300

    commit

--- src/api/firebase.ts
+++ src/api/firebase.ts
@@ -1,7 +1,7 @@
 import firebase from 'firebase';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyAxQlsgkke3eVV2dRwjJ_gY3XMB0bA-w6E',
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: 'my-works-notes-tasks.firebaseapp.com',
   projectId: 'my-works-notes-tasks',
   databaseURL: 'https://my-works-notes-tasks-default-rtdb.europe-west1.firebasedatabase.app',
